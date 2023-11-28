765b6a6095fe64f76fb431348f54fb13e7792491
nattapon-th <nattapon.adwork@gmail.com>
2023-11-28 19:46:45 +0700

    int structure project with repository and service design pattern

---
+++ AssignmentPiSecurities/Controllers/Users/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentPiSecurities.Controllers.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        [Route("CreateOrUpdateUsers")]
+        public async Task<IActionResult> UpdateUsers()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete]
+        [Route("DeleteUsers")]
+        public async Task<IActionResult> DeleteUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pi.Interfaces/Repositories/Users/IUserRepositories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Interfaces.Repositories.Users
+{
+    internal class IUserRepositories
+    {
+    }
+}
