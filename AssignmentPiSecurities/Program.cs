ba09f1fc9e692c709682d7b82882eda991ffe7f8
nattapon-th <nattapon.adwork@gmail.com>
2023-11-28 20:21:12 +0700

    api versioning and add way to select version by request

--- AssignmentPiSecurities/Program.cs
+++ AssignmentPiSecurities/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using System.Net.Sockets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"), // ?api-version = 1.0
+        new HeaderApiVersionReader("X-Version"), // Headers > X-Version = 1.0
+        new MediaTypeApiVersionReader("ver")); // Headers > Accept = application/json; ver=1.0
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
